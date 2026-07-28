2b1762444ad475c7f1d6f67984a94b24d3850133
pmrvdemesquita07 <pmrvdemesquita@gmail.com>
2026-07-28 10:08:55 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const apiRoutes = require('./routes/api');
-
-const app = express();
-app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.json({ name: 'Not Alone Core API', status: 'ok', version: 'v1' });
-});
-
-app.use('/v1', apiRoutes);
+const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
